199037692efc8991dd63df9865493898d4c61f42
Matt Karmazyn <matt@itskarma.wtf>
2023-06-26 18:45:39 -0400

    the ai

--- pages/api/submit/index.js
+++ pages/api/submit/index.js
@@ -1,10 +1,50 @@
-export default function handler(req, res) {
-  const body = req.body;
-  res.status(200).json(body);
-  // TODO:
-  // do some backend validation
-  // send the request to openai chatgpt
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  const body = JSON.parse(req.body);
+
+  // TODO: Do some backend validation
+
+  // Convert the user's input into a prompt
+  const pDays = `Suggest a ${body.days} day vacation destination`;
+  const pPeople = `for ${body.people} people`;
+  const pDestination = `in the ${body.destination}`;
+  const pSource = `from the ${body.source} area`;
+  const pBudget = `for under ${body.budget} USD that`;
+  const pFlight = `${body.flight ? "includes" : "does not include"} a flight,`;
+  const pHotel = `${body.hotel ? "includes" : "does not include"} a hotel,`;
+  const pRental = `${
+    body.rental ? "includes" : "does not include"
+  } a rental car,`;
+  const pIdeas = `as well as 5 ideas of what to do during the stay at the destination?`;
+
+  const prompt = `${pDays} ${pPeople} ${pDestination} ${pSource} ${pBudget} ${pFlight} ${pHotel} ${pRental} ${pIdeas}`;
+
+  // Call the OpenAI API
+  const chatCompletion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "Travel Agent." },
+      { role: "user", content: prompt },
+    ],
+  });
+
+  // log the output to the console
+  console.log(chatCompletion.data.choices[0].message.content);
+  console.log(chatCompletion.data.usage);
+
   // pluck out the details that we can use for travelociy, expedia, etc.
   // generate the affiliate links
   // return the response and the links to the frontend
+
+  const response = {
+    content: chatCompletion.data.choices[0].message.content,
+  };
+
+  res.status(200).json(response);
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 export default function Home() {
   const [budget, setBudget] = useState();
+  const [people, setPeople] = useState();
+  const [days, setDays] = useState();
   const [source, setSource] = useState();
   const [destination, setDestination] = useState();
   const [flight, setFlight] = useState(false);
   const [hotel, setHotel] = useState(false);
   const [rental, setRental] = useState(false);
+  const [responseContent, setResponseContent] = useState();
+  const [loading, setLoading] = useState(false);
 
   function handleSubmit() {
     // e.preventDefault();
@@ -17,9 +21,11 @@ export default function Home() {
     }
 
     const postData = async () => {
-      console.log("clicked");
+      setLoading(true);
       const data = {
         budget,
+        people,
+        days,
         source,
         destination,
         flight,
@@ -33,24 +39,25 @@ export default function Home() {
       });
       return response.json();
     };
-    console.log(postData);
     postData().then((data) => {
-      alert(JSON.stringify(data));
+      console.log(JSON.stringify(data));
+      setResponseContent(data.content);
+      setLoading(false);
     });
   }
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="class flex">
-        <p className="text-4xl font-bold text-center text-gray-900 dark:text-gray-300">
-          Let our AI generate your next trip
-        </p>
-      </div>
+  const displayResponse = (
+    <div className="flex w-full max-w-lg mt-10">
+      <p className="text text-gray-900 dark:text-gray-200">{responseContent}</p>
+    </div>
+  );
 
+  const displayForm = (
+    <div>
       <form className="w-full max-w-lg mt-10">
         {/* Row 1 */}
         <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3 mb-6 md:mb-0">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
               className="block uppercase tracking-wide text-gray-900 dark:text-gray-300 text-xs font-bold mb-2"
               htmlFor="grid-budget"
@@ -67,6 +74,50 @@ export default function Home() {
               onChange={(e) => setBudget(e.target.value)}
             />
             {!budget && (
+              <p className="text-red-500 text-xs italic">
+                Please fill out this field.
+              </p>
+            )}
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-900 dark:text-gray-300 text-xs font-bold mb-2"
+              htmlFor="grid-people"
+            >
+              # of People
+            </label>
+            <input
+              className={`${
+                !people ? "border-red-500" : "border-green-500"
+              } appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
+              id="grid-people"
+              type="text"
+              placeholder="2"
+              onChange={(e) => setPeople(e.target.value)}
+            />
+            {!people && (
+              <p className="text-red-500 text-xs italic">
+                Please fill out this field.
+              </p>
+            )}
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-900 dark:text-gray-300 text-xs font-bold mb-2"
+              htmlFor="grid-days"
+            >
+              # of Days
+            </label>
+            <input
+              className={`${
+                !days ? "border-red-500" : "border-green-500"
+              } appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
+              id="grid-days"
+              type="text"
+              placeholder="3"
+              onChange={(e) => setDays(e.target.value)}
+            />
+            {!days && (
               <p className="text-red-500 text-xs italic">
                 Please fill out this field.
               </p>
@@ -126,11 +177,9 @@ export default function Home() {
               </p>
             )}
             <p className="text-gray-600 text-xs italic">
-              Make it as detailed as you&apos;d like. Examples: <br />
-              &quot;Anywhere in the World&quot; <br />
-              &quot;Anywhere in America&quot; <br />
-              &quot;New York City, NY, USA&quot; <br />
-              &quot;Lower Manhattan&quot;
+              Make it as detailed as you&apos;d like. Examples: &quot;Anywhere
+              in the World&quot;, &quot;Anywhere in America&quot;, &quot;New
+              York City, NY, USA&quot;, &quot;Lower Manhattan&quot;
             </p>
           </div>
         </div>
@@ -201,17 +250,52 @@ export default function Home() {
         <div className="flex justify-center mt-6">
           <button
             className={`${
-              !budget || !source || !destination
+              !budget || !source || !destination || loading
                 ? "opacity-90 cursor-not-allowed bg-blue-500 hover:bg-blue-700"
                 : "bg-green-500 hover:bg-green-700"
             } text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
             type="button"
             onClick={handleSubmit}
           >
-            Find my next adventure
+            {loading ? (
+              <div className="flex inline-flex items-center" role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">loading</span>
+                Finding your adventure...
+              </div>
+            ) : (
+              "Find my next adventure"
+            )}
           </button>
         </div>
       </form>
+    </div>
+  );
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="class flex">
+        <p className="text-4xl font-bold text-center text-gray-900 dark:text-gray-300">
+          Let our AI generate your next trip
+        </p>
+      </div>
+      {/* if responseContent is not null, display the response, else display the form */}
+      {responseContent ? displayResponse : displayForm}
     </main>
   );
 }
